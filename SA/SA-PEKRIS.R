a8df5b0dd461d51ee1702c7db0310af9b6e89fd9
schmidtaar <83698670+schmidtaar@users.noreply.github.com>
2022-02-08 16:30:44 +0100

    Update SA-PEKRIS.R

--- SA/SA-PEKRIS.R
+++ SA/SA-PEKRIS.R
@@ -5,16 +5,16 @@ setwd("C:/Users/bruno/ownCloud/PEKRIS/009-SA")
 df <- read.csv("3-results/SA-PEKRIS.csv",skip=6)
 
 # remove combinations without egg production
-df <- df[df$n_k_max_eggs!=0,]
+df <- df[df$max_eggs_krill!=0,]
 
 # test fo normal distributions
-lm1 <- lm(n_s_max_total~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(n_s_max_total~(vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 shapiro.test(resid(lm1))
 
 # ANOVA proportions for n_s_max_total
 df$n_s_max_total <- rank(df$n_s_max_total)
 
-lm1 <- lm(n_s_max_total~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(n_s_max_total~(vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 # check variance homogeneity
 plot(lm1,which=3)
@@ -30,14 +30,14 @@ g$response <- "n_s_max_total"
 oresult <- g
 
 # trst for normal distribution
-lm1 <- lm(n_s_med~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(median_abundance_salp_overall~(vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 shapiro.test(resid(lm1))
 
-# ANOVA proportions for n_s_med
-df$n_s_med <- rank(df$n_s_med)
+# ANOVA proportions for median_abundance_salp_overall
+df$n_s_med <- rank(df$median_abundance_salp_overall)
 
-lm1 <- lm(n_s_med~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(median_abundance_salp_overall~(vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 # check variance homogeneity
 plot(lm1,which=3)
@@ -49,18 +49,18 @@ row.names(g) <- NULL
 g <- g[with(g,order(-g$prop)),]
 g <- subset(g,prop>=1)
 g <- g[,c(7,6)]
-g$response <- "n_s_med"
+g$response <- "median_abundance_salp_overall"
 oresult <- rbind(oresult,g)
 
 # test for normal distribution
-lm1 <- lm(n_k_max_eggs~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(max_eggs_krill~(vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 shapiro.test(resid(lm1))
 
-# ANOVA proportions for n_k_max_eggs
-df$n_k_max_eggs <- rank(df$n_k_max_eggs)
+# ANOVA proportions for max_eggs_krill
+df$max_eggs_krill <- rank(df$max_eggs_krill)
 
-lm1 <- lm(n_k_max_eggs~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(max_eggs_krill~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 # check variance homogeneity
 plot(lm1,which=3)
@@ -72,18 +72,18 @@ row.names(g) <- NULL
 g <- g[with(g,order(-g$prop)),]
 g <- subset(g,prop>=1)
 g <- g[,c(7,6)]
-g$response <- "n_k_max_eggs"
+g$response <- "max_eggs_krill"
 oresult <- rbind(oresult,g)
 
 # trst for normal distribution
-lm1 <- lm(l_k_max~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(max_length_krill~(vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 shapiro.test(resid(lm1))
 
-# ANOVA proportions for l_k_max
-df$l_k_max <- rank(df$l_k_max)
+# ANOVA proportions for max_length_krill
+df$max_length_krill <- rank(df$max_length_krill)
 
-lm1 <- lm(l_k_max~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(max_length_krill~(vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 # check variance homogeneity
 plot(lm1,which=3)
@@ -95,18 +95,18 @@ row.names(g) <- NULL
 g <- g[with(g,order(-g$prop)),]
 g <- subset(g,prop>=1)
 g <- g[,c(7,6)]
-g$response <- "l_k_max"
+g$response <- "max_length_krill"
 oresult <- rbind(oresult,g)
 
 # test for normal distribution
-lm1 <- lm(d_k_firstrepro~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(age_of_first_reproduction_krill~(vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 shapiro.test(resid(lm1))
 
-# ANOVA proportions for d_k_firstrepro
-df$d_k_firstrepro <- rank(df$d_k_firstrepro)
+# ANOVA proportions for age_of_first_reproduction_krill
+df$d_k_firstrepro <- rank(df$age_of_first_reproduction_krill)
 
-lm1 <- lm(d_k_firstrepro~(vegetation_delay+oozoid_resp+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blasto_resp+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(age_of_first_reproduction_krill~(vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immiprob+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 # check variance homogeneity
 plot(lm1,which=3)
@@ -118,7 +118,7 @@ row.names(g) <- NULL
 g <- g[with(g,order(-g$prop)),]
 g <- subset(g,prop>=1)
 g <- g[,c(7,6)]
-g$response <- "d_k_firstrepro"
+g$response <- "age_of_first_reproduction_krill"
 oresult <- rbind(oresult,g)
 
 oresult <- as.data.frame(oresult)
@@ -145,10 +145,10 @@ oresult$response <- as.factor(oresult$response)
 oresult$parameter <- factor(oresult$parameter,levels=a)
 
 oresult$parameter <- factor(oresult$parameter,levels=c("chla_growth","chla_decay","krill_halfsat",
-                                                       "salp_halfsat","oozoid_resp","salp_mortality",
+                                                       "salp_halfsat","oozoid_respiration","salp_mortality",
                                                        "salp_length","Residuals"))
 
-oresult$response <- factor(oresult$response,levels=c("n_s_max_total","n_s_med","l_k_max","n_k_max_eggs","d_k_firstrepro"))
+oresult$response <- factor(oresult$response,levels=c("n_s_max_total","median_abundance_salp_overall","max_length_krill","max_eggs_krill","age_of_first_reproduction_krill))
 
 levels(oresult$response) <- c("max_abundance_salp_day","median_abundance_salp_overall","max_length_krill",
                               "max_eggs_krill","age_of_first_reproduction_krill")
