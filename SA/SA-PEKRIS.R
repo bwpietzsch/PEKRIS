3982638cada96cc3b7ac914754523d7fc43e0371
bwpietzsch <82039569+bwpietzsch@users.noreply.github.com>
2023-09-21 21:13:06 +0200

    Add files via upload

--- SA/SA-PEKRIS.R
+++ SA/SA-PEKRIS.R
@@ -176,7 +176,7 @@ ggsave("4-plots/SA-anova.pdf",width=5.5,height=4)
 rm(list=ls())
 dev.off()
 
-# revision --------------------------------------------------------------
+# revision1 --------------------------------------------------------------
 
 # set working directory accordingly
 setwd("C:/Users/bruno/ownCloud/PEKRIS/009-SA")
@@ -193,13 +193,13 @@ df <- df[df$sum_eggs_krill!=0,]
 ###
 
 # test fo normal distributions
-lm1 <- lm(max_abundance_salp_season~(chla_supply+vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(max_abundance_salp_season~(chla_supply+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 shapiro.test(resid(lm1))
 
 # rank transformation
 df$max_abundance_salp_season <- rank(df$max_abundance_salp_season)
 
-lm1 <- lm(max_abundance_salp_season~(chla_supply+vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(max_abundance_salp_season~(chla_supply+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 g <- anova(lm1)
 g$prop = round(g$`Sum Sq` / sum(g$`Sum Sq`) * 100,2)
@@ -213,14 +213,14 @@ oresult <- g
 
 ## median abundance salp ##
 # test for normal distribution
-lm1 <- lm(median_abundance_salp_overall~(chla_supply+vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(median_abundance_salp_overall~(chla_supply+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 shapiro.test(resid(lm1))
 
 # rank transformation
 df$median_abundance_salp_overall <- rank(df$median_abundance_salp_overall)
 
-lm1 <- lm(median_abundance_salp_overall~(chla_supply+vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(median_abundance_salp_overall~(chla_supply+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 g <- anova(lm1)
 g$prop = round(g$`Sum Sq` / sum(g$`Sum Sq`) * 100,2)
@@ -234,14 +234,14 @@ oresult <- rbind(oresult,g)
 
 ## krill eggs ##
 # test for normal distribution
-lm1 <- lm(sum_eggs_krill~(chla_supply+vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(sum_eggs_krill~(chla_supply+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 shapiro.test(resid(lm1))
 
 # ANOVA proportions for n_k_max_eggs
 df$sum_eggs_krill <- rank(df$sum_eggs_krill)
 
-lm1 <- lm(sum_eggs_krill~(chla_supply+vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(sum_eggs_krill~(chla_supply+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 g <- anova(lm1)
 g$prop = round(g$`Sum Sq` / sum(g$`Sum Sq`) * 100,2)
@@ -255,13 +255,13 @@ oresult <- rbind(oresult,g)
 
 ## mean length krill
 # test for normal distribution
-lm1 <- lm(mean_length_krill~(chla_supply+vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(mean_length_krill~(chla_supply+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 shapiro.test(resid(lm1))
 
 df$mean_length_krill <- rank(df$mean_length_krill)
 
-lm1 <- lm(mean_length_krill~(chla_supply+vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(mean_length_krill~(chla_supply+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 g <- anova(lm1)
 g$prop = round(g$`Sum Sq` / sum(g$`Sum Sq`) * 100,2)
@@ -275,13 +275,13 @@ oresult <- rbind(oresult,g)
 
 ## abundance krill
 # test for normal distribution
-lm1 <- lm(abundance_krill~(chla_supply+vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(abundance_krill~(chla_supply+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 shapiro.test(resid(lm1))
 
 df$abundance_krill <- rank(df$abundance_krill)
 
-lm1 <- lm(abundance_krill~(chla_supply+vegetation_delay+oozoid_respiration+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+blastozoid_respiration+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
+lm1 <- lm(abundance_krill~(chla_supply+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,df)
 
 g <- anova(lm1)
 g$prop = round(g$`Sum Sq` / sum(g$`Sum Sq`) * 100,2)
@@ -338,6 +338,173 @@ ggplot(oresult,aes(parameter, response)) +
         legend.position = "top")
 
 ggsave("4-plots/revision-SA-anova.pdf",width=5.5,height=4)
+
+# delete everything
+rm(list=ls())
+dev.off()
+
+# revision2 --------------------------------------------------------------
+
+# set working directory accordingly
+setwd("C:/Users/bruno/ownCloud/PEKRIS/009-SA")
+
+# import raw results
+df <- read.csv("3-results/SA-PEKRIS.csv",skip=6)
+
+## max abundance salp ##
+###
+
+# test fo normal distributions
+lm1 <- lm(max_abundance_salp_season~(chla_supply+species+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,
+          df[df$species!="krill",])
+shapiro.test(resid(lm1))
+
+# rank transformation
+df$max_abundance_salp_season <- rank(df$max_abundance_salp_season)
+
+lm1 <- lm(max_abundance_salp_season~(chla_supply+species+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,
+          df[df$species!="krill",])
+
+g <- anova(lm1)
+g$prop = round(g$`Sum Sq` / sum(g$`Sum Sq`) * 100,2)
+g$parameter <- row.names(g)
+row.names(g) <- NULL
+g <- g[with(g,order(-g$prop)),]
+g <- subset(g,prop>=1)
+g <- g[,c(7,6)]
+g$response <- "max_abundance_salp"
+oresult <- g
+
+## median abundance salp ##
+# test for normal distribution
+lm1 <- lm(median_abundance_salp_overall~(chla_supply+species+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,
+          df[df$species!="krill",])
+
+shapiro.test(resid(lm1))
+
+# rank transformation
+df$median_abundance_salp_overall <- rank(df$median_abundance_salp_overall)
+
+lm1 <- lm(median_abundance_salp_overall~(chla_supply+species+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,
+          df[df$species!="krill",])
+
+g <- anova(lm1)
+g$prop = round(g$`Sum Sq` / sum(g$`Sum Sq`) * 100,2)
+g$parameter <- row.names(g)
+row.names(g) <- NULL
+g <- g[with(g,order(-g$prop)),]
+g <- subset(g,prop>=1)
+g <- g[,c(7,6)]
+g$response <- "med_abundance_salp"
+oresult <- rbind(oresult,g)
+
+## krill eggs ##
+# test for normal distribution
+lm1 <- lm(sum_eggs_krill~(chla_supply+species+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,
+          df[df$species!="salps",])
+
+shapiro.test(resid(lm1))
+
+# ANOVA proportions for n_k_max_eggs
+df$sum_eggs_krill <- rank(df$sum_eggs_krill)
+
+lm1 <- lm(sum_eggs_krill~(chla_supply+species+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,
+          df[df$species!="salps",])
+
+g <- anova(lm1)
+g$prop = round(g$`Sum Sq` / sum(g$`Sum Sq`) * 100,2)
+g$parameter <- row.names(g)
+row.names(g) <- NULL
+g <- g[with(g,order(-g$prop)),]
+g <- subset(g,prop>=1)
+g <- g[,c(7,6)]
+g$response <- "krill_eggs"
+oresult <- rbind(oresult,g)
+
+## mean length krill
+# test for normal distribution
+lm1 <- lm(mean_length_krill~(chla_supply+species+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,
+          df[df$species!="salps",])
+
+shapiro.test(resid(lm1))
+
+df$mean_length_krill <- rank(df$mean_length_krill)
+
+lm1 <- lm(mean_length_krill~(chla_supply+species+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,
+          df[df$species!="salps",])
+
+g <- anova(lm1)
+g$prop = round(g$`Sum Sq` / sum(g$`Sum Sq`) * 100,2)
+g$parameter <- row.names(g)
+row.names(g) <- NULL
+g <- g[with(g,order(-g$prop)),]
+g <- subset(g,prop>=1)
+g <- g[,c(7,6)]
+g$response <- "mean_length_krill"
+oresult <- rbind(oresult,g)
+
+## abundance krill
+# test for normal distribution
+lm1 <- lm(abundance_krill~(chla_supply+species+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,
+          df[df$species!="salps",])
+
+shapiro.test(resid(lm1))
+
+df$abundance_krill <- rank(df$abundance_krill)
+
+lm1 <- lm(abundance_krill~(chla_supply+species+vegetation_delay+krill_amount+chla_growth+salp_immigration_probability+salp_mortality+krill_mortality+salp_amount+chla_decay+salp_length+salp_starvation+krill_hibernation+salp_halfsat+krill_halfsat)^2,
+          df[df$species!="salps",])
+
+g <- anova(lm1)
+g$prop = round(g$`Sum Sq` / sum(g$`Sum Sq`) * 100,2)
+g$parameter <- row.names(g)
+row.names(g) <- NULL
+g <- g[with(g,order(-g$prop)),]
+g <- subset(g,prop>=1)
+g <- g[,c(7,6)]
+g$response <- "abundance_krill"
+oresult <- rbind(oresult,g)
+
+oresult <- as.data.frame(oresult)
+
+# round proportion for plotting
+oresult$prop <- round(oresult$prop,digits=0)
+
+# remove parameters with less than 5 %
+oresult <- oresult[oresult$prop>=5,]
+
+# calculate % sum
+ddf <- aggregate(prop~parameter,oresult,sum)
+
+# store order for factor levels of plot
+a <- ddf[order(-ddf$prop),1]
+oresult <- oresult[oresult$parameter!="Residuals",]
+
+# rearrange levels
+oresult$parameter <- factor(oresult$parameter,levels=a)
+
+# oresult$parameter <- factor(oresult$parameter,levels=c("chla_growth","chla_decay","krill_halfsat",
+#                                                        "salp_halfsat","oozoid_resp","salp_mortality",
+#                                                        "salp_length","Residuals"))
+
+
+oresult$response <- factor(oresult$response,levels=c("abundance_krill","krill_eggs","mean_length_krill",
+                                                     "med_abundance_salp","max_abundance_salp"))
+
+# plot data
+library("ggplot2")
+
+ggplot(oresult,aes(parameter, response)) +
+  geom_tile(aes(fill = prop),col="black",na.rm = T) + 
+  geom_text(aes(label = prop),na.rm=T) +
+  scale_fill_gradient(low = "#A9E2F3", high = "#3A7EAB",name="variance [%]") +
+  labs(x="",y="") +
+  theme_bw() +
+  theme(axis.text.x = element_text(colour="black",angle = 45, hjust = 1),
+        axis.text.y = element_text(colour="black"),
+        legend.position = "top")
+
+ggsave("4-plots/revision2-SA-anova.pdf",width=5.5,height=4)
 
 # delete everything
 rm(list=ls())
